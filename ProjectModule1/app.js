25c9b3ab6bd1d6593ac6f06060794b8b6b8fcaac
Duyz1 <110105863+laiquangduy@users.noreply.github.com>
2022-08-23 10:03:57 +0700

    Add files via upload

--- ProjectModule1/app.js
+++ ProjectModule1/app.js
@@ -4,11 +4,48 @@ function myFunction() {
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
-    btnText.innerHTML = `<i class="fa-solid fa-angle-down"></i>See more`;
+    btnText.innerHTML = `<div id="round-btn">
+    <i class="fa-solid fa-angle-down"></i>
+  </div>
+  <span>See more</span>
+  </div>
+`;
     moreText.style.display = "none";
   } else {
     dots.style.display = "none";
-    btnText.innerHTML = `<i class="fa-solid fa-angle-up"></i>See less`;
+    btnText.innerHTML = `<div id="round-btn">
+    <i class="fa-solid fa-angle-up"></i>
+  </div>
+  <span>See less</span>
+  `;
     moreText.style.display = "inline";
   }
 }
+
+// round = document.getElementById("round-btn");
+// console.log(round);
+// round.onmouseover = function (event) {
+//   round.style.backgroundcolor = "#D8DADF";
+// };
+
+// function changecolor() {
+//   round = document.getElementById("round-btn");
+//   round.style.backgrounColor = "#D8DADF";
+// }
+
+var btnText1 = document.getElementById("myBtn");
+var see = document.getElementById("s");
+var ic = document.getElementById("c");
+
+btnText1.onmouseover = function (event) {
+  event.target.children[0].style.backgroundColor = "#D8DADF";
+
+  console.log(event.target.children);
+  console.log(event.target.children.children);
+};
+
+btnText1.onmouseout = function (event) {
+  event.target.children[0].style.backgroundColor = "#E4E6EB";
+
+  console.log(event.target.children);
+};
